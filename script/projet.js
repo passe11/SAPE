b7b8be02a3c1eb87b2dad90b5e7a710a37995967
Pascal <pmarl012@uottawa.ca>
2020-10-19 15:47:17 -0400

    Ajout de la page de projet et rafinement du login

--- script/projet.js
+++ script/projet.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+
     document.getElementById("name").innerHTML=localStorage.getItem("name");
     document.getElementById("name2").innerHTML=localStorage.getItem("name")
     document.getElementById("name3").innerHTML=localStorage.getItem("name");;
@@ -7,9 +8,6 @@ $( document ).ready(function() {
     document.getElementById("disponible").innerHTML= disponible;
     document.getElementById("cour").innerHTML=localStorage.getItem("class");
     document.getElementById("description").innerHTML=localStorage.getItem("description");
-    document.getElementById("bookImage").src=localStorage.getItem("image");
-    document.getElementById("modalImage").src=localStorage.getItem("image");
-    document.getElementById("modalImage2").src=localStorage.getItem("image");
     if(disponible==0){
         document.getElementById("bookingButton").style.display="none";
         document.getElementById("waitListButton").style.display="inline-block";
